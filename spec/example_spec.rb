4f0c45a989cf708d612369a53b07f1d4d7cdbbfc
ksaito <kitsit.39@gmail.com>
2024-02-29 08:53:09 +0900

    比較

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe do
       expect(1).not_to eq(2)
     end
   end
+
+  describe 'comparison' do
+    it 'comparison', :aggregate_failures do
+      expect(9).to be > 7
+      expect(9).to be >= 9
+      expect(9).to be < 10
+    end
+  end
 end
